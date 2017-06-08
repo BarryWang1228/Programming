e39d4f5fa12d90d9099255264ff6f62a08381553
barry_wang <barry_wang@htc.com>
2017-06-08 12:01:12 +0800

    Algorithms #7 Reverse Integer Barry write by C
    Result:
    1032 / 1032 test cases passed.
    Status: Accepted
    Runtime: 19 ms

---
+++ LeetCode_Algorithms/0007_Reverse Integer_Barry.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int reverse(int x)
+{
+    /* To avoid overflow while calculate, use double size int to save the input value. */
+    long out = 0, xx = (long)x;
+    int MAX_INT = 0x7FFFFFFF, MIN_INT = 0x80000000;
+    /* Use while loop to go-through all the input number of bits */
+    while(xx)
+    {
+        out = out * 10 + xx % 10;
+        xx = xx / 10;
+    }
+    /* Checking if the reverse value is overflow of int(4 bytes) */
+    if( out <= MAX_INT && out >= MIN_INT )
+    {
+        return (int)out;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int main()
+{
+    int input_num, rev_num;
+    printf("Please input a int value: \n");
+    scanf("%d", &input_num);
+    rev_num = reverse(input_num);
+    printf("The reverse of the input number is: %d\n", rev_num);
+    system("PAUSE");
+    return 0;
+}
